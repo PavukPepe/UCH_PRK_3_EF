cde1b0499bee7352ee15fef2ba8ab09ee06bc154
pvk <sergpilk33@gmail.com>
2024-03-19 10:30:13 +0300

    v1

---
+++ PRACT_LAB_1_EF/MainWindow.xaml.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace PRACT_LAB_1_EF
+{
+    /// <summary>
+    /// Логика взаимодействия для MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void second_table_Click(object sender, RoutedEventArgs e)
+        {
+            table_frame.Content = new Employeers_page();
+        }
+    }
+}
